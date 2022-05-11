f8de3a026b76965f5398da665c6639446c50bee8
jummi <98qkrwjdal@naver.com>
2022-05-11 15:43:53 +0900

    6-5 Account 영속성 어댑터 구현

---
+++ src/main/java/com/woowa/cleanarchitecture/account/adapter/out/persistence/AccountPersistenceAdapter.java
@@ -0,0 +1,46 @@
+package com.woowa.cleanarchitecture.account.adapter.out.persistence;
+
+import com.woowa.cleanarchitecture.account.application.port.out.LoadAccountPort;
+import com.woowa.cleanarchitecture.account.application.port.out.UpdateAccountStatePort;
+import com.woowa.cleanarchitecture.account.domain.Account;
+import com.woowa.cleanarchitecture.account.domain.AccountId;
+import com.woowa.cleanarchitecture.account.domain.Activity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class AccountPersistenceAdapter implements LoadAccountPort, UpdateAccountStatePort {
+    private final AccountRepository accountRepository;
+    private final ActivityRepository activityRepository;
+    private final AccountMapper accountMapper;
+
+    @Override
+    public Account loadAccount(AccountId accountId, LocalDateTime baselineDate) {
+        AccountJpaEntity account = accountRepository.findById(accountId.id())
+                .orElseThrow(EntityNotFoundException::new);
+
+        List<ActivityJpaEntity> activities = activityRepository.findByOwnerAccountIdAndSince(accountId.id(), baselineDate);
+        Long withdrawalBalance = orZero(activityRepository.getWithdrawalBalanceUntil(accountId.id(), baselineDate));
+        Long depositBalance = orZero(activityRepository.getDepositBalanceUntil(accountId.id(), baselineDate));
+
+        return accountMapper.mapJpaEntityToDomainEntity(account, activities, withdrawalBalance, depositBalance);
+    }
+
+    @Override
+    public void updateActivities(Account account) {
+        for (Activity activity : account.getActivityWindow().activities()) {
+            if (activity.activityId() == null) {
+                activityRepository.save(accountMapper.mapDomainEntityToJpaEntity(activity));
+            }
+        }
+    }
+
+    private Long orZero(Long value) {
+        return value == null ? 0L : value;
+    }
+}
