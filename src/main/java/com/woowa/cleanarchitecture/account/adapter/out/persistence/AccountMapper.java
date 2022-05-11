c574b434ab0afb88a9d99824f09c7c9278995c4e
jummi <98qkrwjdal@naver.com>
2022-05-11 15:43:37 +0900

    6-5 Account, Activity의 Jpa Entity <-> Domain Entity 매퍼

---
+++ src/main/java/com/woowa/cleanarchitecture/account/adapter/out/persistence/AccountMapper.java
@@ -0,0 +1,51 @@
+package com.woowa.cleanarchitecture.account.adapter.out.persistence;
+
+import com.woowa.cleanarchitecture.account.domain.*;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AccountMapper {
+    Account mapJpaEntityToDomainEntity(AccountJpaEntity account,
+                                       List<ActivityJpaEntity> activities,
+                                       Long withdrawalBalance,
+                                       Long depositBalance) {
+        Money baselineBalance = Money.subtract(
+                Money.of(depositBalance),
+                Money.of(withdrawalBalance)
+        );
+
+        return Account.withId(
+                new AccountId(account.getId()),
+                baselineBalance,
+                new ActivityWindow(activities.stream()
+                        .map(this::mapJpaEntityToDomainEntity)
+                        .collect(Collectors.toList())
+                )
+        );
+    }
+
+    Activity mapJpaEntityToDomainEntity(ActivityJpaEntity activity) {
+        return new Activity(
+                new ActivityId(activity.getId()),
+                new AccountId(activity.getOwnerAccountId()),
+                new AccountId(activity.getSourceAccountId()),
+                new AccountId(activity.getTargetAccountId()),
+                Money.of(activity.getAmount()),
+                activity.getTimestamp()
+        );
+    }
+
+    ActivityJpaEntity mapDomainEntityToJpaEntity(Activity activity) {
+        return new ActivityJpaEntity(
+                activity.activityId().id(),
+                activity.ownerAccountId().id(),
+                activity.sourceAccountId().id(),
+                activity.targetAccountId().id(),
+                activity.money().amount().longValue(),
+                activity.timestamp()
+        );
+    }
+}
