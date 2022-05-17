c2cba035a28d246f9098d416e170631fff5fa0a0
jummi <98qkrwjdal@naver.com>
2022-05-18 01:33:37 +0900

    feat: AccountLock interface 구현체

---
+++ src/main/java/com/woowa/cleanarchitecture/account/adapter/out/persistence/NoOpAccountLock.java
@@ -0,0 +1,18 @@
+package com.woowa.cleanarchitecture.account.adapter.out.persistence;
+
+import com.woowa.cleanarchitecture.account.application.port.out.AccountLock;
+import com.woowa.cleanarchitecture.account.domain.AccountId;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NoOpAccountLock implements AccountLock {
+    @Override
+    public void lockAccount(AccountId accountId) {
+        // do nothing
+    }
+
+    @Override
+    public void releaseAccount(AccountId accountId) {
+        // do nothing
+    }
+}
